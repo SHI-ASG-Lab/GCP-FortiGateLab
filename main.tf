4de08be69d48e6762d0e8f033018bd913eeee719
jawilliamstx <79536884+jawilliamstx@users.noreply.github.com>
2021-07-16 18:33:25 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -86,44 +86,6 @@ data "google_compute_subnetwork" "fg1-2-sn" {
   project = var.gcpProject
 }
 
-# Ubuntu System
-/*
-data "google_compute_image" "ubuntu1" {
-  name  = "fortilab1-ubuntu"
-  project = var.gcpProject
-}
-
-resource "google_compute_disk" "ubuntu1-disk" {
-  name = "fortilab1-ubuntu-disk"
-  description = "OS disk made from image"
-  image = data.google_compute_image.ubuntu1.self_link
-  zone = var.gcpZone
-}
-
-resource "google_compute_address" "ubuntu-1-ip" {
-  name = "ubuntu-1-ip"
-  address_type = "EXTERNAL"
-}
-
-resource "google_compute_instance" "Ubuntu_vm" {
-  project      = var.gcpProject
-  name         = "fortilab-ubuntu-1"
-  machine_type = "e2-medium"
-  zone         = var.gcpZone
-  boot_disk {
-    source     = google_compute_disk.ubuntu1-disk.self_link
-  }
-  network_interface {
-    network    = data.google_compute_network.fg1-1-net.self_link
-    subnetwork = data.google_compute_subnetwork.fg1-1-sn.self_link
-    access_config {
-      nat_ip = google_compute_address.ubuntu-1-ip.address
-    }
-  }
-  labels = local.fg1Labels
-  tags  = local.netTags
-}
-*/
 # FortiGate
 
 data "google_compute_image" "fg-ngfw" {
