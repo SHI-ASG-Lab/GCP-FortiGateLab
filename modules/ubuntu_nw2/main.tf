45adfb4a4aa174714c6b80f60ffe0fa1ea4126f7
jawilliamstx <79536884+jawilliamstx@users.noreply.github.com>
2021-07-16 14:44:49 -0500

    Update main.tf

--- modules/ubuntu_nw2/main.tf
+++ modules/ubuntu_nw2/main.tf
@@ -1,25 +1,24 @@
 # Creating (#?) of Ubuntu VMs based on image.
 
-data "google_compute_image" "ubuntu1" {
+data "google_compute_image" "ubuntu2" {
   name  = "fortilab1-ubuntu"
   project = var.gcpProject
 }
 
 resource "google_compute_disk" "ubuntu1-disk" {
-  name = "fortilab1-ubuntu-disk"
-  description = "OS disk made from image"
-  image = data.google_compute_image.ubuntu1.self_link
+  name = var.disk1Name
+  image = data.google_compute_image.ubuntu2.self_link
   zone = var.gcpZone
 }
 
-resource "google_compute_address" "ubuntu-1-ip" {
-  name = "ubuntu-1-ip"
+resource "google_compute_address" "ubuntu-2-ip" {
+  name = "ubuntu-2-ip"
   address_type = "EXTERNAL"
 }
 
 resource "google_compute_instance" "Ubuntu_vm" {
   project      = var.gcpProject
-  name         = "fortilab-ubuntu-1"
+  name         = var.ub2Name
   machine_type = "e2-medium"
   zone         = var.gcpZone
   boot_disk {
@@ -29,7 +28,7 @@ resource "google_compute_instance" "Ubuntu_vm" {
     network    = data.google_compute_network.fg1-2-net.self_link
     subnetwork = data.google_compute_subnetwork.fg1-2-sn.self_link
     access_config {
-      nat_ip = google_compute_address.ubuntu-1-ip.address
+      nat_ip = google_compute_address.ubuntu-2-ip.address
     }
   }
   labels = local.fg1Labels
