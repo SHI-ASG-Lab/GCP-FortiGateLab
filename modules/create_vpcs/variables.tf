b64615c15cabcc0d6ca1d4e62f372301465de239
jawilliamstx <79536884+jawilliamstx@users.noreply.github.com>
2021-07-16 20:22:26 -0500

    Create variables.tf

---
+++ modules/create_vpcs/variables.tf
@@ -0,0 +1,41 @@
+# Variables
+/*
+var.project_name
+var.region  gcpZone
+var.subnet_cidr1
+var.subnet_cidr2
+var.fgint1
+var.fgint2
+*/
+
+variable "gcpProject" {
+  type = string
+}
+
+variable "gcpZone" {
+  type = string
+}
+
+variable "labels" {
+  type = map(string)
+}
+
+variable "tags" {
+  type = set(string)
+}
+
+variable "subnet_cidr1" {
+  type = string
+}
+
+variable "subnet_cidr2" {
+  type = string
+}
+
+variable "fgint1" {
+  type = string
+}
+
+variable "fgint2" {
+  type = string
+}
