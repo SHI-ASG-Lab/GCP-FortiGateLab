eb16758c78cffc95edff0096c85740cc5c627b08
jawilliamstx <79536884+jawilliamstx@users.noreply.github.com>
2021-07-19 13:56:30 -0500

    Update main.tf

--- modules/create_vpcs/main.tf
+++ modules/create_vpcs/main.tf
@@ -1,12 +1,12 @@
 # Create VPC for Network1
 resource "google_compute_network" "vpc1" {
- name                    = "${var.project_name}-1-net"                                          ####var
+ name                    = "${var.projectName}-1-net"                                          ####var
  auto_create_subnetworks = false
 }
 
 # Create Subnet for Network1
 resource "google_compute_subnetwork" "subn1" {
- name          = "${var.project_name}-1-sn"
+ name          = "${var.projectName}-1-sn"
  ip_cidr_range = var.subnet_cidr1                                          ####var
  network       = google_compute_network.vpc1.self_link #data.google_compute_network.vpc1.self_link
  region        = var.gcpZone                                          ####var
@@ -14,13 +14,13 @@ resource "google_compute_subnetwork" "subn1" {
 
 # Create VPC for Network2
 resource "google_compute_network" "vpc2" {
- name                    = "${var.project_name}-2-net"                                          ####var
+ name                    = "${var.projectName}-2-net"                                          ####var
  auto_create_subnetworks = false
 }
 
 # Create Subnet for Network2
 resource "google_compute_subnetwork" "subn2" {
- name          = "${var.project_name}-2-sn"                                          ####var
+ name          = "${var.projectName}-2-sn"                                          ####var
  ip_cidr_range = var.subnet_cidr2                                          ####var
  network       = google_compute_network.vpc2.self_link #data.google_compute_network.vpc2.self_link
  region        = var.gcpZone                                          ####var
@@ -30,7 +30,7 @@ resource "google_compute_subnetwork" "subn2" {
 
 # VPC1 firewall configuration
 resource "google_compute_firewall" "firewall1" {
-  name    = "${var.project_name}-firewall"                                          ####var
+  name    = "${var.projectName}-firewall"                                          ####var
   network = google_compute_network.vpc1.self_link #data.google_compute_network.vpc1.self_link 
 
   allow {
@@ -59,7 +59,7 @@ resource "google_compute_firewall" "firewall1" {
 
 # VPC2 firewall configuration
 resource "google_compute_firewall" "firewall2" {
-  name    = "${var.project_name}-firewall2"                                          ####var
+  name    = "${var.projectName}-firewall2"                                          ####var
   network = google_compute_network.vpc2.self_link #data.google_compute_network.vpc2.self_link
 
   allow {
@@ -88,7 +88,7 @@ resource "google_compute_firewall" "firewall2" {
 
 #Routes
 resource "google_compute_route" "fgnw1to2" {
-  name        = "${project_name}-fgnw1to2"
+  name        = "${projectName}-fgnw1to2"
   dest_range  = var.subnet_cidr2                                          ####var
   network     = google_compute_network.vpc1.self_link #data.google_compute_network.vpc1.self_link
   next_hop_ip = var.fgint1                                          ####var
@@ -96,7 +96,7 @@ resource "google_compute_route" "fgnw1to2" {
 }
 
 resource "google_compute_route" "fgnw2to1" {
-  name        = "${project_name}-fgnw2to1"
+  name        = "${projectName}-fgnw2to1"
   dest_range  = var.subnet_cidr1                                          ####var
   network     = google_compute_network.vpc2.self_link #data.google_compute_network.vpc2.self_link
   next_hop_ip = var.fgint2                                          ####var
