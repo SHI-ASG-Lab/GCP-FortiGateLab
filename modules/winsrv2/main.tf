e91528472965f9c1ffe3b2c9b9edf296898b5e8c
jawilliamstx <79536884+jawilliamstx@users.noreply.github.com>
2021-07-23 18:40:44 -0500

    Update main.tf

--- modules/winsrv2/main.tf
+++ modules/winsrv2/main.tf
@@ -33,7 +33,7 @@ resource "google_compute_instance" "winsrv_vm" {
     network    = var.network2
     subnetwork = var.subnetwork2
     access_config {
-      nat_ip = google_compute_address.winsrv-2-ip.address
+      nat_ip = google_compute_address.winsrv-2-ip
     }
   }
   labels = var.labels
