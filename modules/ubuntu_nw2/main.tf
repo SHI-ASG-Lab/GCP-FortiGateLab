be77f990651098465de03d1e475ddf830db0ae02
jawilliamstx <79536884+jawilliamstx@users.noreply.github.com>
2021-07-23 19:05:14 -0500

    Update main.tf

--- modules/ubuntu_nw2/main.tf
+++ modules/ubuntu_nw2/main.tf
@@ -29,7 +29,7 @@ resource "google_compute_instance" "Ubuntu_vm" {
     network    = var.network2
     subnetwork = var.subnetwork2
     access_config {
-      nat_ip = google_compute_address.ubuntu-2-ip
+      nat_ip = google_compute_address.ubuntu-2-ip.address
     }
   }
   labels = var.labels
