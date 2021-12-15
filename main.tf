353c82b56de1ca22fc9834976064d022ca4fbf56
j-isley <79328510+j-isley@users.noreply.github.com>
2021-12-15 13:46:43 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -48,39 +48,39 @@ variable "gcpZone" {
 }
 variable "customerAbv" {
   type = string
-  default = "labtest"
+#  default = "labtest"
 }
 variable "ubnw1Count" {
   type = number
-  default = 1
+#  default = 1
 }
 variable "ubnw2Count" {
   type = number
-  default = 1
+#  default = 1
 }
 variable "win1Count" {
   type = number
-  default = 1
+#  default = 1
 }
 variable "win2Count" {
   type = number
-  default = 1
+#  default = 1
 }
 variable "subnet_cidr1" {
   type = string
-  default = "10.0.110.0/24"
+ # default = "10.0.110.0/24"
 }
 variable "subnet_cidr2" {
   type = string
-  default = "10.0.111.0/24"
+#  default = "10.0.111.0/24"
 }
 variable "fgint1" {
   type = string
-  default "10.0.110.2"
+#  default "10.0.110.2"
 }
 variable "fgint2" {
   type = string
-  default = "10.0.111.2"
+#  default = "10.0.111.2"
 }
 
 # Locals
