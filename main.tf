78ba6d32316405bfd8394df05d67d6d2fca6870a
jawilliamstx <79536884+jawilliamstx@users.noreply.github.com>
2023-09-22 17:26:00 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -117,7 +117,7 @@ variable "gcp_service_list" {
     "cloudresourcemanager.googleapis.com",
     "appengine.googleapi.com",
     "appengineflex.googleapi.com",
-    #"cloudbuild.googleapi.com",
+    "cloudbuild.googleapi.com",
     "serviceusage.googleapi.com",
     "iam.googleapi.com"
   ]
